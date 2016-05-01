84df50b895089036b39f365fb7df4a850bac4cb3
Aleksandar Ristic <aleksandar.r@gmx.at>
2016-05-01 20:55:06 +0200

    done

--- Demo.Client/MainWindow.xaml.cs
+++ Demo.Client/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Configuration;
+using System.Windows;
 using Demo.Proxies;
 
 namespace Demo.Client
@@ -8,6 +9,10 @@ namespace Demo.Client
         public MainWindow()
         {
             InitializeComponent();
+            var configVersion = ConfigurationSettings.AppSettings["VER_NR"];
+            var version = string.IsNullOrWhiteSpace(configVersion) ? null : configVersion;
+
+            DemoClient.Version = version;
         }
 
         private void BtnVer1Clicked(object sender, RoutedEventArgs e)
@@ -15,11 +20,6 @@ namespace Demo.Client
             var proxy = new DemoClient();
             proxy.DoSomething();
             proxy.Close();
-        }
-
-        private void BtnVer2Clicked(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
--- Demo.Proxies/DemoClient.cs
+++ Demo.Proxies/DemoClient.cs
@@ -1,10 +1,28 @@
 ﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
 using Demo.Contracts;
 
 namespace Demo.Proxies
 {
     public class DemoClient : ClientBase<IDemoService>, IDemoService
     {
+        public static string Version { get; set; }
+
+        public DemoClient()
+        {
+            // spin the operation scope
+            // note: if not doing this at first then 
+            // the OperationContext.Current will be NULL
+            var operationScope = new OperationContextScope(InnerChannel);
+
+            // inject version value into soap message header
+            if (!string.IsNullOrWhiteSpace(Version))
+            {
+                OperationContext.Current.OutgoingMessageHeaders.Add(
+                    MessageHeader.CreateHeader("VER_NR", "http://pingo/version", Version));
+            }
+        }
+
         public void DoSomething()
         {
             Channel.DoSomething();
